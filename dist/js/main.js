318324a05ee91d1f23d5f90d029d10a687c7fa79
Christopher Silich <chrissilich@gmail.com>
2018-05-29 15:13:05 -0400

    in class minor updates

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * The master todo list controller
+ * Keeps all the items in an array, and updates the DOM as needed
+ * 
+ */
+var TodoList = function TodoList() {
+  _classCallCheck(this, TodoList);
+};
+
+/**
+ * One todo list item
+ * Keeps track of it's own state (text, done, etc)
+ * Updates it's own internal DOM as needed
+ */
+
+
+var TodoItem = function TodoItem(text) {
+  _classCallCheck(this, TodoItem);
+};
+
+var todoList = new TodoList();
+var todoItem1 = new TodoItem("Milk");
+var todoItem2 = new TodoItem("Eggs");
+var todoItem3 = new TodoItem("Diapers");
+//# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+/**
+ * The master todo list controller
+ * Keeps all the items in an array, and updates the DOM as needed
+ * 
+ */
 class TodoList {
 	constructor() {
 		
@@ -5,7 +10,11 @@ class TodoList {
 }
 
 
-
+/**
+ * One todo list item
+ * Keeps track of it's own state (text, done, etc)
+ * Updates it's own internal DOM as needed
+ */
 class TodoItem {
 	constructor(text) {
 
